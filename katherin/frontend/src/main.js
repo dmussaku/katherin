0ad3b5864012fe7cbf9fba3012c70abde4667cba
dmussaku <dmussaku@gmail.com>
2017-02-19 16:38:07 +0600

    Add router and start simple BLog component on frontend

--- katherin/frontend/src/main.js
+++ katherin/frontend/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Blog from './apps/blog/Blog.vue'
+import VueRouter from 'vue-router'
 
-/* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    { path: '/', component: Blog }
+  ]
+})
+
+// Start the app on the #app div
+
 new Vue({
+	router,
   el: '#app',
   template: '<App/>',
   render: h => h(App),
