74dfdc085c345cbad098febccc78832a11f024dd
amir <amir@easyanticheat.net>
2017-02-19 13:15:55 +0200

    [+] Adds API for authenticating users

--- katherin/backend/apps/api_v1/apis/auth.py
+++ katherin/backend/apps/api_v1/apis/auth.py
@@ -2,9 +2,12 @@
 """
 API for authorithing/authenticating requests
 """
+from django.contrib import auth
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.exceptions import PermissionDenied
 
 from .serializers.auth_serializer import *
 
@@ -13,7 +16,27 @@ class AuthIndex(APIView):
     """
     Handles requests coming to /api/v1/auth/ - user authentication
     """
+    # This API endpoint has to circumvent default SessionAuthentication for all requests
+    permission_classes = (AllowAny, )
+
     def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if not serializer.is_valid():
+            content = {
+                'success': False,
+                'errors': serializer.errors
+            }
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        email = serializer.validated_data.get('email')
+        password = serializer.validated_data.get('password')
+        user = auth.authenticate(email=email, password=password)
+
+        if user is not None and user.is_active:
+            auth.login(request, user)
+        else:
+            # Don't return any sensible 
+            raise PermissionDenied
         content = {
             'success': True
         }
--- katherin/backend/apps/api_v1/tests/test_auth_api.py
+++ katherin/backend/apps/api_v1/tests/test_auth_api.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import json
+
 from rest_framework.test import APITestCase, APIClient
 
 from apps.users.models import CustomUser
@@ -17,8 +19,35 @@ class TestPostAuthIndex(APITestCase):
     def test_data_validation(self):
         self.assertEqual(CustomUser.objects.count(), 3)
 
+    def test_invalid_data_submitted(self):
+        response = self.client.post('/api/v1/auth/', follow=True)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.data, 
+            {
+                'errors': {
+                    'email': ['This field is required.'], 
+                    'password': ['This field is required.']
+                    },
+                'success': False
+            }
+            )
+
     def test_fail_authentication(self):
-        pass
+        response = self.client.post(
+            '/api/v1/auth/',
+            json.dumps({'email': 'amir@katherin.com', 'password': 'Random password'}),
+            content_type='application/json',
+            follow=True
+            )
+        self.assertEqual(response.status_code, 403)
 
     def test_successful_authentication(self):
-        pass
+        # Known fixtures password for all users 'qweasdzxc'
+        response = self.client.post(
+            '/api/v1/auth/',
+            json.dumps({'email': 'amir@katherin.com', 'password': 'qweasdzxc'}),
+            content_type='application/json',
+            follow=True
+            )
+        self.assertEqual(response.status_code, 200)
