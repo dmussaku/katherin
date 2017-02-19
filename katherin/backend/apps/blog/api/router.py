8ac0d1797cd789c1f74545e0fc71a67040a3acae
dmussaku <dmussaku@gmail.com>
2017-02-19 20:54:59 +0600

    Register post urls in router

--- katherin/backend/apps/blog/api/router.py
+++ katherin/backend/apps/blog/api/router.py
@@ -2,7 +2,9 @@
 from rest_framework import routers
 
 from .article.api import ArticleViewSet
+from .post.api import PostViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'blog/articles', ArticleViewSet, base_name='articles')
+router.register(r'blog/posts', PostViewSet, base_name='posts')
